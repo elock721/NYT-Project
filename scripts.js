6712b3e8c76f486e018d9f276e7e1113d9e6a61e
Michael Cui <cuimichael500@gmail.com>
2020-06-26 14:22:48 -0500

    api

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
 $("#searchBtn").on("click", function(){
 
     var text = $("#searchText").val();
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+text+"election&api-key=CrOfebTO4nxToR74F4U1rSaXCYFBQXqh"
 
-    console.log(text);
+    fetch(url)
+    .then(function(response) {
+    
+    //Only proceed if valid city
+    if (response.ok) {
+        
+        $.ajax({
+            url: url,
+            method: "GET"
+          }).then(function(data) {
+
+            console.log(data);
+            
+          });
+        }
+
+    });
 
 });
+
+
+
 
